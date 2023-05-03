cd82950e29fb9095f89628a61e34e94fdeb741b3
ical-Jeon <junbs95@gmail.com>
2023-05-03 09:40:58 +0900

    export library in a big config file

--- los_keeper_ros2/los_server/LosServer.hpp
+++ los_keeper_ros2/los_server/LosServer.hpp
@@ -1,8 +1,11 @@
 #ifndef HEADER_LOSSERVER
 #define HEADER_LOSSERVER
-
+#include <los_keeper/wrapper/Wrapper.hpp>
 namespace los_keeper {
-class LosServer {};
+class LosServer {
+private:
+  Wrapper wrapper_;
+};
 
 } // namespace los_keeper
 #endif /* HEADER_LOSSERVER */
