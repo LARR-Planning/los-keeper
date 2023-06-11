6da52fe3581706d9d82a6ecb17a9d713578eed16
Felipe Jeon <junbs95@gmail.com>
2023-06-11 21:23:46 +0900

    Add expiration triggering and test (#44)

--- los_keeper/src/wrapper/store.cc
+++ los_keeper/src/wrapper/store.cc
@@ -6,9 +6,10 @@ Action DecideAction(const State &state) {
   if (!state.is_activated || !state.is_data_received)
     return Action::kIdle;
   if (!state.is_currently_safe) {
+    // TODO(@): take whatever action to respond to drone unsafe status
     return Action::kStop;
   }
-  if (!state.is_planning_expired || (!state.is_planning_safe || !state.is_planning_visible))
+  if (state.is_planning_expired || (!state.is_planning_safe || !state.is_planning_visible))
     return Action::kReplan;
   return Action::kIdle;
 }
